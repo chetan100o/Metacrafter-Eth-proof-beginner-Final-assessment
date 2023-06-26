232201258dfbdfb1bb07f47b8bbe8fb007554b29
chetan100o <137300850+chetan100o@users.noreply.github.com>
2023-06-26 15:19:50 +0530

    Create ethproof.sol

---
+++ ethproof.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.8.18;
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "SetKal";
+    string public tokenAbbvr = "KAL";
+    uint public totalSupply = 0;
+
+
+    // mapping variable here
+    mapping(address=>uint) public tokenHolders;
+
+
+    // mint function
+    function mint(address _address, uint _value) public{
+        totalSupply+=_value;
+        tokenHolders[_address] += _value;
+    }
+
+
+    // burn function
+    function burn(address _address, uint _value) public{
+        require(tokenHolders[_address] > _value, "Cannot burn more than balance tokens");
+        totalSupply-=_value;
+        tokenHolders[_address] -= _value;
+    }
+
+}
